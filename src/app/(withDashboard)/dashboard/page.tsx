3a04840ea88d0caf504c5a9c7d07d51d2fc7d757
Jamil-Aktar-Ifat <jamilaktarifat@hotmail.com>
2025-08-25 12:12:22 +0100

    fetched a logged in user from the session

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,25 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  // console.log(session);
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+      <h1 className="text-4xl text-center mt-10">
+        Welcome {session?.user?.name}
+      </h1>
+      <h1 className="text-4xl text-center mt-10">
+        Email: {session?.user?.email}
+      </h1>
+      <Image
+        src={session?.user?.image || "/default-profile.png"}
+        width={200}
+        height={200}
+        alt="${session?.user?.name}"
+        className="mx-auto"
+      ></Image>
     </div>
   );
 };
