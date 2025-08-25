a9ea59b1cf53dc7974c0198f90d9e64fbc107e1f
Jamil-Aktar-Ifat <jamilaktarifat@hotmail.com>
2025-08-26 00:08:52 +0100

    google authentication system implemented

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -4,22 +4,31 @@ import Image from "next/image";
 
 const DashboardPage = async () => {
   const session = await getServerSession(authOptions);
-  // console.log(session);
+
+  if (!session) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold">No user is logged in</h1>
+        <p className="text-lg mt-2">Please sign in to access dashboard.</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1 className="text-4xl text-center mt-10">
-        Welcome {session?.user?.name}
+        Welcome {session.user?.name}
       </h1>
       <h1 className="text-4xl text-center mt-10">
-        Email: {session?.user?.email}
+        Email: {session.user?.email}
       </h1>
       <Image
-        src={session?.user?.image || "/default-profile.png"}
+        src={session.user?.image || "/default-profile.png"}
         width={200}
         height={200}
-        alt="${session?.user?.name}"
+        alt={session.user?.name || "Default profile"}
         className="mx-auto"
-      ></Image>
+      />
     </div>
   );
 };
